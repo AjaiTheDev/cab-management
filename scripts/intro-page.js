0860107865740fa2b4f97964a86a6889efe06ee2
Ajai Anil <ajai.anil@experionglobal.local>
2023-01-10 12:32:31 +0530

    first commit

---
+++ scripts/intro-page.js
@@ -0,0 +1,14 @@
+function signInFunction() {
+
+    window.location.href = 'file:///C:/Cab%20Management%20system/layout/login-page.html';
+}
+
+function rideSignUpFunction() {
+
+    window.location.href = 'file:///C:/Cab%20Management%20system/layout/registration-page.html';
+}
+
+function driverSignUpFunction() {
+
+    window.location.href = 'file:///C:/Cab%20Management%20system/layout/driver-registration.html';
+}
